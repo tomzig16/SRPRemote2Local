6ce183390cd70cea2f059638cc0ad91a0d502b8f
tomzig16 <tzigmantavicius@gmail.com>
2020-08-10 23:27:58 +0300

    Implement main

--- srpr2l.py
+++ srpr2l.py
@@ -1,23 +1,19 @@
 import argparse
 import src.HelpMessages as HelpMes
 
-def TestFunc(args):
+def ReplaceManifestJson(projectPath, srpPath):
     print("Test func was hit!\n")
+    print(srpPath)
+
+
+if __name__ == "__main__":
+    mainParser = argparse.ArgumentParser(description=HelpMes.appDescription)
+    mainParser.add_argument("--setup", nargs=1, help=HelpMes.setSrpPath, default=None)
+    mainParser.add_argument("project_path", nargs="?", help="project path should be here")
+    mainParser.add_argument("srp_path", nargs="?", help="srp path should be here")
+    args = mainParser.parse_args(["arg1"])
     print(args)
-
-
-mainParser = argparse.ArgumentParser(description=HelpMes.appDescription)
-commandGroups = mainParser.add_subparsers(title="commandGroups")
-
-installationSubParser = commandGroups.add_parser("--setup", help=HelpMes.setSrpPath)
-installationSubParser.set_defaults(func=TestFunc)
-installationSubParser.add_argument("srp_path", nargs=1, help=HelpMes.setSrpPath)
-
-executionSubParser = commandGroups.add_parser("-e", help="launch (optional)")
-mainParser.add_argument("project_path", nargs="?", help="project path should be here")
-mainParser.add_argument("srp_path", nargs="?", help="srp path should be here")
-
-
-mainParser.print_help()
-
-
+    if args.setup != None:
+        print("Setup is not none")
+    else:
+        ReplaceManifestJson(args.project_path, args.srp_path)
