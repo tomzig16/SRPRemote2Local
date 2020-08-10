7b57562e1b8da76d97b71610b084431466901664
tomzig16 <tzigmantavicius@gmail.com>
2020-08-10 22:57:07 +0300

    Add arguments for argparse

--- srpr2l.py
+++ srpr2l.py
@@ -1,1 +1,23 @@
-import argparse 
+import argparse
+import src.HelpMessages as HelpMes
+
+def TestFunc(args):
+    print("Test func was hit!\n")
+    print(args)
+
+
+mainParser = argparse.ArgumentParser(description=HelpMes.appDescription)
+commandGroups = mainParser.add_subparsers(title="commandGroups")
+
+installationSubParser = commandGroups.add_parser("--setup", help=HelpMes.setSrpPath)
+installationSubParser.set_defaults(func=TestFunc)
+installationSubParser.add_argument("srp_path", nargs=1, help=HelpMes.setSrpPath)
+
+executionSubParser = commandGroups.add_parser("-e", help="launch (optional)")
+mainParser.add_argument("project_path", nargs="?", help="project path should be here")
+mainParser.add_argument("srp_path", nargs="?", help="srp path should be here")
+
+
+mainParser.print_help()
+
+
