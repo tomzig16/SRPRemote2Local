1ead7e1d9f9dfb1fae94e40f313a8812c7a4d832
tomzig16 <tzigmantavicius@gmail.com>
2020-08-11 21:46:08 +0300

    Add rest of help messages

--- src/HelpMessages.py
+++ src/HelpMessages.py
@@ -1,3 +1,4 @@
 appDescription = "SRP Remote to Local (or SRPR2L) is a tool that updates project manifest.json file contents (in effect replacing or adding) to include local SRP paths."
-
-
+setSrpPath = "Set local SRP path that will later be used in the projects"
+projectPath = "Path to the project folder which needs packages manifest to be changed to use local SRP"
+srpPath = "(Optional) Path to local SRP project (in case if it is not set or different version than default is being used)"
