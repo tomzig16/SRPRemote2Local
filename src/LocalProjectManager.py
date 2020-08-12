96e7b48f30ab0f4edca86c5909a6efe7b06afecb
tomzig16 <tzigmantavicius@gmail.com>
2020-08-12 23:05:40 +0300

    Enable package manifest replacement

--- src/LocalProjectManager.py
+++ src/LocalProjectManager.py
@@ -30,19 +30,31 @@ def GenerateSRPPackagesPaths(srpPath):
 
 
 
-def GetProjectPackageManifestData(srpPath):
+def GetProjectPackageManifestData(projectPath):
     manifestPath = "Packages/manifest.json" # Unity searches for manifest exactly there (inside project folder)
-    fullManifestPath =  os.path.realpath(os.path.join(srpPath, manifestPath))
+    fullManifestPath = os.path.realpath(os.path.join(projectPath, manifestPath))
     try:
-        manifestData = None
         with open(fullManifestPath) as jsonFile:
-            pass
-    except:
-        pass
+            return json.load(jsonFile)
+    except FileNotFoundError:
+        return None
 
 
-def ReplaceManifestJson(projectPath, srpPath):
+def UpdateProjectManifestJson(projectJson, localSRPData):
+    for key, val in localSRPData.items():
+        projectJson["dependencies"][key] = val
+
+def SaveNewProjectManifest(projectPath, newManifest):
+    manifestPath = "Packages/manifest.json"
+    fullManifestPath = os.path.realpath(os.path.join(projectPath, manifestPath))
+    with open(fullManifestPath, "w") as projMan:
+        json.dump(newManifest, projMan)
+
+def AddLocalSRPToProject(projectPath, srpPath):
     localSRPData = None
+    # First check if custom SRP path is given and valid
+    # if it is given it is higher priority to be used than local saved SRP folder 
+    # (this is in case we have 2 different branches at once etc.)
     if srpPath != None:
         if os.path.exists(srpPath) == False:
             print("Given SRP path does not exist.\nCheck given SRP path and try again...")
@@ -50,14 +62,23 @@ def ReplaceManifestJson(projectPath, srpPath):
             return
         localSrpPath = os.path.realpath(srpPath)
         localSRPData = GenerateSRPPackagesPaths(localSrpPath)
-    else:
+    else: 
         localSrpPath = GetLocalSRPPath()
         if localSrpPath == None:
             if srpPath == None:
                 print("No SRP path was given. Please run command with --setup or give SRP path alongside project path.")
                 return
     
-    # Else if local srp path is set correctly
-    srpJson = GetProjectPackageManifestData(localSrpPath)
+    # Else - local srp path is set correctly
+    projectManifest = GetProjectPackageManifestData(projectPath)
+    if projectManifest == None:
+        print("Packages/manifest.json was not found. Make sure given project path (destination) is Unity project.")
+        return
+    
+    UpdateProjectManifestJson(projectManifest, localSRPData)
+    SaveNewProjectManifest(projectPath, projectManifest)
+
+    print("Done")
+
 
             
--- srpr2l.py
+++ srpr2l.py
@@ -1,15 +1,15 @@
 import argparse
 import src.HelpMessages as HelpMes
-import src.LocalDataLoader as ldloader
+import src.LocalProjectManager as ldloader
 
 if __name__ == "__main__":
     mainParser = argparse.ArgumentParser(description=HelpMes.appDescription)
     mainParser.add_argument("--setup", nargs=1, help=HelpMes.setSrpPath, default=None)
     mainParser.add_argument("project_path", nargs="?", help=HelpMes.projectPath)
     mainParser.add_argument("srp_path", nargs="?", help=HelpMes.srpPath)
-    args = mainParser.parse_args(["arg1", "E:\\work\\srp"])
+    args = mainParser.parse_args(["E:/work/unity_proj/emptyproj", "E:\\work\\srp"])
     print(args)
     if args.setup != None:
         print("Setup is not none")
     else:
-        ldloader.ReplaceManifestJson(args.project_path, args.srp_path)
+        ldloader.AddLocalSRPToProject(args.project_path, args.srp_path)
