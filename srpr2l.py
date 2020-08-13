d276835a251f17566169cc62b1aded7efe251b57
tomzig16 <tzigmantavicius@gmail.com>
2020-08-13 23:13:36 +0300

    Remove debug prints and implement help

--- srpr2l.py
+++ srpr2l.py
@@ -9,10 +9,12 @@ if __name__ == "__main__":
     mainParser.add_argument("project_path", nargs="?", help=HelpMes.projectPath)
     mainParser.add_argument("srp_path", nargs="?", help=HelpMes.srpPath)
     mainParser.add_argument("-pa", "--print-actions", action="store_true", help=HelpMes.printActions)
-    args = mainParser.parse_args(["--setup", "E:\\work\\srp"])
-    print(args)
+    args = mainParser.parse_args()
     if args.setup != None:
         SetupLocalSRP(args.setup[0])
     else:
-        printActions = args.print_actions
-        AddLocalSRPToProject(args.project_path, args.srp_path)
+        if args.project_path == None: # required argument if --setup is not used
+            mainParser.print_help()
+        else:
+            printActions = args.print_actions
+            AddLocalSRPToProject(args.project_path, args.srp_path)
